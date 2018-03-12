51ced4afe23d77b113d3e3d0c69a035e2a90786e
monika <mstempniak@gmail.com>
2018-03-12 08:55:47 +0100

    add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports={
+  entry: "./js/app.js",
+  output: {
+    filename: "./js/out.js"
+  },
+  watch: true,
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['env']
+      }
+    },{
+      test: /\.css$/,
+      loader: ['style-loader', 'css-loader']
+    },{
+      test: /\.scss$/,
+      loader: ['style-loader', 'css-loader', 'sass-loader']
+    }]
+  }
+}
