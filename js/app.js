e3fcbf1337556ba0f4a0a906cd0fb88e666df08a
monika <mstempniak@gmail.com>
2018-03-16 12:28:41 +0100

    update

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
   import $ from 'jquery';
   import { NasaData } from './loadNasa';
   import { MarsData } from './loadMars';
-  require('../css/style.scss');
+  require('../scss/style.scss');
 
 $(() => {
 
