87cdb26f3bb64d39c1a4f22740bd5a633eeb4c13
monika <mstempniak@gmail.com>
2018-03-19 00:22:55 +0100

    add banner-progress class & gallery-progress class to spinner

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ $(() => {
   // variables
   const apiKey = 'fnNoKmHzREJJoo7DI4rX1HseOV8UinHXG34A5Pbp';
   const urlNasa = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
-  const urlMars = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${apiKey}`;
+  const urlMars = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&api_key=${apiKey}`;
   const hiddenGallery = $('.gallery-content').last();
 
   // load Nasa data to banner section
@@ -27,7 +27,6 @@ $(() => {
   // show all gallery photos
   const showGallery = $('.gallery-btn').on('click', function(e){
     e.preventDefault();
-    console.log('nananan');
     hiddenGallery.show();
     $(this).parent().hide();
   });
